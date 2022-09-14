94f4b17a61fbe4838a73cf1ccf2db8f2d68a8e88
OverlordP <piero.roca.g@uni.pe>
2022-09-13 22:31:43 -0500

    agrgando metodos post, put, delete

--- cartas/urls.py
+++ cartas/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from cartas.views import *
 
 urlpatterns = [
-    path('', allCartas),
+    path('', allCartas, name="cartas"),
+    path('form/', setCartas ,name="forms"),
     path('<int:id>/', oneCarta),
+    path('forms/', postCartas, name="setforms"),
+    path('delete/<int:id>/', deleteCartas, name="deletecarta"),
+    path('update/<int:id>/', updateCartas, name="updatecarta"),
+    path('formsid/', updateCarta, name="updateCartaid"),
 ]
--- cartas/views.py
+++ cartas/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from unicodedata import name
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cartas3
 # Create your views here.
 def allCartas(request):
@@ -6,4 +7,37 @@ def allCartas(request):
     return render(request, 'cartas.html',{ "cartas" : cartas1})
 
 def oneCarta(request,id):
-    return render(request, 'carta.html')
+
+    carta = get_object_or_404(Cartas3, id=id)
+    return render(request, 'carta.html', { "carta" : carta})
+
+def setCartas(request):
+    return render(request, 'postCartas.html')
+
+def postCartas(request):
+    titulo = request.POST['titulo']
+    nombre = request.POST['name']
+    descripcion = request.POST['description']
+    url = request.POST['Url']
+    modelo = Cartas3(title = titulo, name= nombre, description= descripcion, Url = url)
+    modelo.save()
+    return redirect ('/cartas/')
+
+def deleteCartas(request,id):
+    carta= Cartas3.objects.get(id=id)
+    carta.delete()
+    return redirect ('/cartas/')
+
+def updateCartas(request,id):
+    carta = get_object_or_404(Cartas3, id=id)
+    return render(request, 'updateCarta.html', {"carta": carta})
+
+def updateCarta(request):
+    
+    titulo = request.POST['titulo']
+    nombre = request.POST['name']
+    descripcion = request.POST['description']
+    url = request.POST['Url']
+    modelo = Cartas3(id = request.POST['idt'],title = titulo, name= nombre, description= descripcion, Url = url)
+    modelo.save()
+    return redirect ('/cartas/')
